20c185db57bb6f4a2d53cb2eb0da7e6e364512bd
Karina <73380467+izm-ka@users.noreply.github.com>
2024-05-28 21:01:36 +0700

    final update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -23,6 +23,7 @@ public:
     void NewProjectAudioProcessorEditor::styleMenuChanged();
     void NewProjectAudioProcessorEditor::openSettings(Button* button);
     void NewProjectAudioProcessorEditor::changeTheme(const String& themeName);
+    void NewProjectAudioProcessorEditor::setEffect(const juce::String& effectName);
     void NewProjectAudioProcessorEditor::setupSliderAndLabel(Slider& slider, Label& label, const String& componentID,
         int x, int y, int width, int height, int textBoxWidth, int labelHeight);
 
@@ -39,11 +40,24 @@ private:
     Slider sliderDistortion;
     Slider sliderBlend;
 
+    Slider sliderReverb1;
+    Slider sliderReverb2;
+    Slider sliderReverb3;
+    Slider sliderReverb4;
+    Slider sliderReverb5;
+    Slider sliderReverb6;
+
     Label delayLabel;
     Label feedbackLabel;
     Label volumeLabel;
     Label distortionLabel;
     Label blendLabel;
+
+    Label feedback;
+    Label mix;
+    Label delayTime;
+    Label hpcutoffFrequency;
+    Label lpcutoffFrequency;
 
     ImageButton settingsButton;
 
@@ -51,13 +65,14 @@ private:
 
     Font customFont;
 
-    String currentTheme = "cats";
+    String currentTheme = "basic";
+    String currentEffect = "basic effects";
+
+    void setupBasicEffects();
+    void setupReverbEffects();
 
     TextButton button{ "RESET" };
     ComboBox styleMenu;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(NewProjectAudioProcessorEditor)
 };
-
-
-
